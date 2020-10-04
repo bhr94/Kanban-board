12404bdde48aa8c7c58b39fee001ca1931c71a96
Bahar Mutadayin <baharmutadayin@Bahars-MacBook-Pro.local>
2020-10-04 16:31:35 -0700

    Updates

---
+++ precache-manifest.dd763d239f9adeb85644b0f1d5c144a1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e81e076f590b8d120201be6fcf308e1",
+    "url": "/kanban_board/index.html"
+  },
+  {
+    "revision": "d4f8f5565f8ca50e5f99",
+    "url": "/kanban_board/static/css/2.e55b4e31.chunk.css"
+  },
+  {
+    "revision": "4a59af4030970b83d628",
+    "url": "/kanban_board/static/css/main.2eecc1d4.chunk.css"
+  },
+  {
+    "revision": "d4f8f5565f8ca50e5f99",
+    "url": "/kanban_board/static/js/2.636fad44.chunk.js"
+  },
+  {
+    "revision": "5b001f3e5a51e744ab29cbfcded75f9e",
+    "url": "/kanban_board/static/js/2.636fad44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a59af4030970b83d628",
+    "url": "/kanban_board/static/js/main.b40c444b.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/kanban_board/static/js/main.b40c444b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8361d46df7de213ad1",
+    "url": "/kanban_board/static/js/runtime-main.3083f1fd.js"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/kanban_board/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/kanban_board/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/kanban_board/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/kanban_board/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/kanban_board/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/kanban_board/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2b213388ad8f83f9d573f92bc3a59409",
+    "url": "/kanban_board/static/media/girl-work-on-laptop-1792788-1519329.2b213388.png"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/kanban_board/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/kanban_board/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/kanban_board/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "f48153110985eb3dd39429d6cdc26438",
+    "url": "/kanban_board/static/media/image1.f4815311.png"
+  },
+  {
+    "revision": "6847af5564f7b0abf94316fb8052d527",
+    "url": "/kanban_board/static/media/image2.6847af55.png"
+  },
+  {
+    "revision": "8d3eabc55f28d882f556c57b0614624e",
+    "url": "/kanban_board/static/media/image3.8d3eabc5.png"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/kanban_board/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/kanban_board/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/kanban_board/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/kanban_board/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/kanban_board/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/kanban_board/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
